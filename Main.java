527ef5ef062f9657e53efb955af933347a3f503a
nat_soloviova <natsolovyova2@gmail.com>
2023-03-30 20:40:33 +0300

    small refactoring

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Cat cat = new Cat(); //створити екземпляр другого класу
-        System.out.println(String.format("~~~ <%d> \"~~~", cat.printInfo())); // викликати метод та вивести
+        System.out.printf("~~~ <%d> \"~~~%n", cat.printInfo()); // викликати метод та вивести
         // у консоль “~~~ <повернене значення> “~~~”.
     }
 }
